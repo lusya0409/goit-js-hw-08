e20fb4d47024ebd49e799f3ddecbb345756578a1
lusya0409 <lusya0409@gmail.com>
2023-06-16 13:11:19 +0300

    change 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,6 +3,26 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
-const a = 10;
-console.log(a);
-console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+            <a class="gallery__link" href="${original}" >
+                <img class="gallery__image"
+                    src="${preview}"
+                    alt="${description}"
+                />
+            </a>
+        </li>`
+  )
+  .join('');
+
+galleryList.innerHTML = markup;
+
+const lightbox = new SimpleLightbox('.gallery__link', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
