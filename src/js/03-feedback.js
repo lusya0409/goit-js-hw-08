f49d1cb3b58661cda7be18177c34344c8c68aa3b
lusya0409 <lusya0409@gmail.com>
2023-06-19 13:40:39 +0300

    hw08 made

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+formEl.addEventListener('input', throttle(onFormInput, 500));
+formEl.addEventListener('submit', onFormSubmit);
+
+const formState = getFormState();
+// console.log(formState);
+if (!!formState) {
+  formEl.elements.email.value = formState.email;
+  formEl.elements.message.value = formState.message;
+}
+
+function onFormInput() {
+  const formValues = {
+    email: formEl.elements.email.value,
+    message: formEl.elements.message.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formValues));
+  //   console.log(formValues);
+}
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  console.log(getFormState());
+
+  formEl.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+
+function getFormState() {
+  return JSON.parse(localStorage.getItem('feedback-form-state'));
+}
